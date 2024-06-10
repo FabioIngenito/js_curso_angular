8db31a94b2f94be8f269cf0c519c941a52d884de
Fabio Ingenito <fabio.ingenito@hotmail.com>
2024-06-10 10:27:01 -0300

    feat: Eventos - Exercício 1 - Mudar a cor de fundo da página inteira.

--- Aula_5/eventos/src/app/componentes/eventos/eventos.component.ts
+++ Aula_5/eventos/src/app/componentes/eventos/eventos.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-eventos',
@@ -10,9 +10,34 @@ import { Component } from '@angular/core';
 })
 export class EventosComponent {
   mostrar: boolean = false;
+  bgColor = '#ffffff'; // Cor de fundo inicial
+
+  constructor(private elementRef: ElementRef) {}
+  ngAfterViewInit() {
+    this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor =
+      this.bgColor;
+  }
 
   mostrarMensagem(): void {
     //console.log('Método chamado');
     this.mostrar = !this.mostrar;
+  }
+
+  changeBackgroundColor(): void {
+    const randomColor = this.getRandomColor();
+    this.bgColor = randomColor;
+    this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor =
+      this.bgColor;
+  }
+
+  private getRandomColor(): string {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+
+    return color;
   }
 }
